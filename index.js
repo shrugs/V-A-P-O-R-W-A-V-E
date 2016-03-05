cdf37c0ae28678d688cfb76e133caf901f7dc036
Matt Condon <mattgcondon@gmail.com>
2016-03-05 17:15:45 -0500

    add procfile

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 
+var EMOJI_REGEX = /:\w+:/g;
+
+String.prototype.insertAt = function(index, string) {
+  return this.substr(0, index) + string + this.substr(index);
+}
+
 var stringFormUtils = require('string-form-utils');
 var bodyParser = require('body-parser')
 var express = require('express');
@@ -6,7 +12,20 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/slack', function (req, res) {
-  var vw = stringFormUtils.transformToFullwidth(req.body.text || '')
+  var inputString = (req.body.text || '');
+  var m, matches = [];
+  while (m = EMOJI_REGEX.exec(inputString)) {
+    matches.push(m);
+  }
+  inputString = inputString.split('');
+  for (var match in matches) {
+    inputString.splice(match.index, match[0].length)
+  }
+  console.log(matches);
+  var vw = stringFormUtils.transformToFullwidth(inputString.join(''))
+  for (var match in matches) {
+    vw.insertAt(match.index, match[0])
+  }
   res.send(vw);
 });
 
