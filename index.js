608672f5137d73cf796fa67b1f13b55b08e7fa4e
Matt Condon <mattgcondon@gmail.com>
2019-04-14 17:07:34 -0700

    feat: convert to now/node lambda function (#5)

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
-
 var EMOJI_REGEX = /:[\w\-\_]+:/g;
 
 String.prototype.insertAt = function(index, string) {
   return this.substr(0, index) + string + this.substr(index);
-}
+};
 
-var stringFormUtils = require('string-form-utils');
-var bodyParser = require('body-parser')
-var express = require('express');
-var app = express();
+const stringFormUtils = require("string-form-utils");
+const bodyParser = require("body-parser");
+const express = require("express");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.post('/slack', function (req, res) {
-  var inputString = (req.body.text || '');
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post("*", (req, res) => {
+  if (req.body == null) {
+    return res.status(400).send({ error: "no JSON object in the request" });
+  }
+
+  var inputString = req.body.text || "";
 
   var indexShift = 0;
-  var m, matches = [];
-  while (m = EMOJI_REGEX.exec(inputString)) {
+  var m,
+    matches = [];
+  while ((m = EMOJI_REGEX.exec(inputString))) {
     m.shiftIndex = m.index - indexShift;
     indexShift += m[0].length;
     matches.push(m);
   }
-  inputString = inputString.split('');
+  inputString = inputString.split("");
   matches.forEach(function(match) {
-    inputString.splice(match.shiftIndex, match[0].length)
-  })
+    inputString.splice(match.shiftIndex, match[0].length);
+  });
 
-  var vw = stringFormUtils.transformToFullwidth(inputString.join(''))
+  var vw = stringFormUtils.transformToFullwidth(inputString.join(""));
   matches.forEach(function(match) {
-    vw = vw.insertAt(match.index, match[0])
-  })
+    vw = vw.insertAt(match.index, match[0]);
+  });
 
   res.json({
-    response_type: 'in_channel',
+    response_type: "in_channel",
     text: vw,
   });
 });
 
-app.get('/', function(req, res) {
-  res.send('OK');
+app.all("*", (req, res) => {
+  res.status(405).send({ error: "only POST requests are accepted" });
 });
 
-var server = app.listen(process.env.PORT || 3000, function () {
-  var port = server.address().port;
-  console.log('Example app listening on post :%s', port);
-});
+module.exports = app;
